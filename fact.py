73c1973eb74ff47e43aeb92e97a0f7dcd844d8f3
ver00in1ika <155071272+shanghaihark@users.noreply.github.com>
2025-11-28 22:40:13 +0200

    Create fact.py

---
+++ fact.py
@@ -0,0 +1,11 @@
+import random
+
+facts = [
+    "Bananas are berries.",
+    "Honey never spoils.",
+    "Octopuses have three hearts.",
+    "The Moon has moonquakes.",
+    "Some turtles breathe through their butts."
+]
+
+print(random.choice(facts))
